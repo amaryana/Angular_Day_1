6db1b7e0a673008ac3ce6ace4eaa5bc007d385f9
amaryana <amarpatel1290@gmail.com>
2016-04-08 14:35:13 -0600

    serving correct folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = 80;
 
-app.use(express.static(__dirname+'/../newChats'));
+app.use(express.static(__dirname+'/../public'));
 
 app.listen(port, function(){
  console.log("Listeing on port ", port);
